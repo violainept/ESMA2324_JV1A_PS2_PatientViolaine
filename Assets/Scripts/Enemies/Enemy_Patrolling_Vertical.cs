43bbde649a543a096720edb44fa4edc59905d27b
Violaine <violaine.patient@gmail.com>
2024-06-05 12:37:42 +0200

    Add Animations Enemies

--- Assets/Scripts/Enemies/Enemy_Patrolling_Vertical.cs
+++ Assets/Scripts/Enemies/Enemy_Patrolling_Vertical.cs
@@ -7,10 +7,15 @@ public class Enemy_Patrolling_Vertical : MonoBehaviour
 {
     [Header("GameObject")]
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator anim;
 
     [Header("Detection")]
     private bool top;
 
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
     public void Update()
     {
         if (top)
@@ -21,13 +26,14 @@ public class Enemy_Patrolling_Vertical : MonoBehaviour
         if (!top)
         {
             StartCoroutine(gravityActivated());
+            
         }
     }
 
     // Permet de desactiver la gravite au bout d'un certain temps
     public IEnumerator gravityDesactivated()
     {
-        rb.gravityScale = -10;
+        rb.gravityScale = -3;
         yield return new WaitForSeconds(2);
         top = false;
     }
@@ -35,18 +41,24 @@ public class Enemy_Patrolling_Vertical : MonoBehaviour
     // Permet d'activer la gravite au bout d'un certain temps
     public IEnumerator gravityActivated()
     {
-        rb.gravityScale = 10;
+        rb.gravityScale = 3;
         yield return new WaitForSeconds(2);
         top = true;
     }
 
     // Permet de tuer le Joueur s'il rentre en contact avec l'ennemi
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player"))
         {
             Player_Controller player = other.transform.GetComponent<Player_Controller>();
             player.Die();
         }
+    }
+    private void Rotation()
+    {
+        Vector3 ScalerUP = transform.localScale;
+        ScalerUP.y *= -1;
+        transform.localScale = ScalerUP;
     }
 }
